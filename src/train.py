8dcbb033012576bde65e4165431e8084b32c41e6
K-Musty <kmustapha9564@gmail.com>
2026-09-07 07:00:48 +0100

    Removed Verbose from optimizer

--- src/train.py
+++ src/train.py
@@ -174,7 +174,7 @@ def main(args):
     # Optimizer and scheduler
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode='max', factor=0.5, patience=5, verbose=True
+        optimizer, mode='max', factor=0.5, patience=5
     )
 
     scaler = GradScaler()
